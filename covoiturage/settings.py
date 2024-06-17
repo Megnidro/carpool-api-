50fea10425a79eb9b3cec7098ff257f233917ca9
Dominique MEGNIDRO [ Bénin Diplomatie ] <dmegnidro@gouv.bj>
2024-06-18 01:08:47 +0200

    free commit

--- covoiturage/settings.py
+++ covoiturage/settings.py
@@ -232,7 +232,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 15,
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ]
 }
 import dj_database_url
