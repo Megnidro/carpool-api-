cd1021b9f255893a7a4cdb0815c2b98e57332b4b
Dominique MEGNIDRO [ Bénin Diplomatie ] <dmegnidro@gouv.bj>
2024-06-18 01:22:51 +0200

    free commit

--- courses/permissions.py
+++ courses/permissions.py
@@ -1,4 +1,16 @@
+from rest_framework.permissions import BasePermission
 
 
-def permissions(user):
-    pass
+class IsDriverOrBoth(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role in ['driver', 'both']
+
+
+class IsPassengerOrBoth(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role in ['passenger', 'both']
+
+
+class IsPassenger(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.role == 'passenger'
